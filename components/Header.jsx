391e4e9792212ef863b9d7b375ef854c08a6599e
Aman Raj Singh <68102965+aman220@users.noreply.github.com>
2023-12-01 19:12:50 +0530

    First Commit
    add repo commit

---
+++ components/Header.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { getCategories } from "../services";
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+          <div class="container flex flex-wrap items-center justify-between mx-auto">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl ">Graph CMS</span>
+          </Link>
+            <button
+              data-collapse-toggle="navbar-default"
+              type="button"
+              class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+              <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                
+              <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle  ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+          ))}
+        </div>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        {/* <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl ">Graph CMS</span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle  ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+          ))}
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
